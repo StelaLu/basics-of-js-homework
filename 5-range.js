e1d351d9d6d78bb3b0d08c86788d87320d41630a
Stela Luchianciuc [Intern] <Stela.Luchianciuc@endava.com>
2021-09-20 17:37:14 +0300

    Solution for Range

--- 5-range.js
+++ 5-range.js
@@ -15,11 +15,32 @@
  *
  */
 
-// Your code here.
-//
-// console.log(range(1, 10));
-// → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-// console.log(range(5, 2, -1));
-// → [5, 4, 3, 2]
-// console.log(sum(range(1, 10)));
-// → 55
+console.log('Array containing all the numbers from start up to (and including) end');
+let range = (start, end, step = start < end ? 1 : -1) => {
+    let arr = [];
+    if (start <= end) {
+        for (let i = start; i <= end; i += step) {
+        arr.push(i);
+        }
+    } else {
+        for (let i = start; i >= end; i += step) {
+            arr.push(i);
+        }
+    }
+    return arr;
+  }
+  
+  console.log(range(2, 4));
+  console.log(range(5, 2, -1));
+
+console.log('Sum that takes an array of numbers and returns the sum of these numbers');
+let sum = (arr) => {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+     sum += arr[i];
+    }
+    
+    return sum;
+  }
+
+console.log(sum(range(1, 10)));
